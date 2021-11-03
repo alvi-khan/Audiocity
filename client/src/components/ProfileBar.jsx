c4cbb629bbae70225ca67a67a032c7c9009155a8
Fida <n.fida6941@gmail.com>
2021-11-03 11:44:52 +0600

    Created log in dialog.

--- client/src/components/ProfileBar.jsx
+++ client/src/components/ProfileBar.jsx
@@ -1,14 +1,32 @@
 import React, { Component } from "react";
 import "../stylesheets/ProfileBar.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import ProfileDialog from "./ProfileDialog";
 
 class Profile extends React.Component {
+  state = {
+    profileDialogShow: false,
+  };
+
   render() {
     return (
-      <button class="profileBar">
-        <i class="icon bi bi-person"></i>
-        Profile
-      </button>
+      <div>
+        <button
+          class="profileBar"
+          onClick={() => {
+            this.setState({ profileDialogShow: true });
+          }}
+        >
+          <i class="icon bi bi-person"></i>
+          Profile
+        </button>
+        <ProfileDialog
+          show={this.state.profileDialogShow}
+          onHide={() => {
+            this.setState({ profileDialogShow: false });
+          }}
+        />
+      </div>
     );
   }
 }
