b43d1da832ffb03783030846aaad5747e154cf62
Fida <n.fida6941@gmail.com>
2021-11-04 21:08:03 +0600

    Added user logout

--- client/src/components/ProfileBar.jsx
+++ client/src/components/ProfileBar.jsx
@@ -8,9 +8,27 @@ class Profile extends React.Component {
     profileDialogShow: false,
   };
 
+  logout = () => {
+    this.props.updateUser("");
+    localStorage.setItem("user", "");
+  };
+
   render() {
-    return (
-      <div>
+    var button;
+    if (this.props.user != "" && this.props.user != null) {
+      button = (
+        <button
+          class="profileBar"
+          onClick={() => {
+            this.logout();
+          }}
+        >
+          <i class="icon bi bi-person"></i>
+          Log Out
+        </button>
+      );
+    } else {
+      button = (
         <button
           class="profileBar"
           onClick={() => {
@@ -20,15 +38,22 @@ class Profile extends React.Component {
           <i class="icon bi bi-person"></i>
           Profile
         </button>
-        <ProfileDialog
-          show={this.state.profileDialogShow}
-          loggedInUser={this.props.user}
-          updateUser={(newUser) => this.props.updateUser(newUser)}
-          onHide={() => {
-            this.setState({ profileDialogShow: false });
-          }}
-        />
-      </div>
+      );
+    }
+    return (
+      <React.Fragment>
+        {button}
+        <div>
+          <ProfileDialog
+            show={this.state.profileDialogShow}
+            loggedInUser={this.props.user}
+            updateUser={(newUser) => this.props.updateUser(newUser)}
+            onHide={() => {
+              this.setState({ profileDialogShow: false });
+            }}
+          />
+        </div>
+      </React.Fragment>
     );
   }
 }
